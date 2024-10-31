ef235f969632b0b17a6a6ae14612a3b77ded6f86
Ital023 <contadoitalo911@gmail.com>
2024-10-31 18:21:07 -0300

    feat: initial files

---
+++ src/main/java/io/github/Ital023/FastFeet/FastFeetApplication.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.FastFeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Ital023/FastFeet/FastFeetApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.FastFeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
